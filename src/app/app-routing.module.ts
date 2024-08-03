c0cb0acd3a4bba4c0d7a78f846db447e7b238876
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-08-04 01:44:50 +0300

    creating Loading Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SplashComponent } from './Components/splash/splash.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/splash',
+    pathMatch:'full' //mathc full path
+  },
+  {
+    path:"splash",
+    component:SplashComponent
+  },
+  {
+    path:"**",
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashComponent } from './Components/splash/splash.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
